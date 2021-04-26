1a0a68fa0f47a15a7201b84bb830bff3b8f9d290
Evgenii Gurin <egurin@luxoft.com>
2021-04-26 20:16:03 +0300

    Add AbstractFunction

---
+++ src/main/java/guldilin/function/AbstractFunction.java
@@ -0,0 +1,6 @@
+package guldilin.function;
+
+public abstract class AbstractFunction implements Calculable {
+    public static double ACCURACY = 1e-4;
+    public static final int MAX_ITERATIONS = 100_000;
+}
