ed9770affd95cd9beeda9a12dc35bd6dcaddda11
Evgenii Gurin <egurin@luxoft.com>
2021-04-26 20:40:49 +0300

    Rename some, add degree test

--- src/test/java/guldilin/function/trig/QuarterTest.java
+++ src/test/java/guldilin/function/trig/QuarterTest.java
@@ -1,16 +1,28 @@
 package guldilin.function.trig;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Quarter tests")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class QuarterTest {
 
     @ParameterizedTest
     @CsvFileSource(resources = "/trig_radian_quarter_data.csv")
-    public void defileRadianQuarter(double x, int expectedQuarter) {
-        Quarter actualQuarter = Quarter.of(x);
+    public void quarterRadianTest(double radian, int expectedQuarter) {
+        Quarter actualQuarter = Quarter.of(radian);
+        assertEquals(expectedQuarter, actualQuarter.ordinal());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/trig_degree_quarter_data.csv")
+    public void quarterDegreeTest(double degree, int expectedQuarter) {
+        double radian = degree / 180 * Math.PI;
+        Quarter actualQuarter = Quarter.of(radian);
         assertEquals(expectedQuarter, actualQuarter.ordinal());
     }
 }
