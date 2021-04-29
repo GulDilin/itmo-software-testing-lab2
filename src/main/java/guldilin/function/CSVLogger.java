3cfa1a1ad1ec5e68c78625bab856be0cb2f2ff7d
GulDilin <zhenyagurin@gmail.com>
2021-04-29 09:50:57 +0300

    Add CSV logger

---
+++ src/main/java/guldilin/function/CSVLogger.java
@@ -0,0 +1,45 @@
+package guldilin.function;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class CSVLogger {
+
+    private String filePath;
+    private String separator;
+    private double step;
+    private double start;
+    private double end;
+
+    public CSVLogger(String fileName, double start, double end, double step, String separator) {
+        this.setFilePath(fileName);
+        this.separator = separator;
+        this.start = start;
+        this.end = end;
+        this.step = step;
+    }
+
+    public void setFilePath(String fileName) {
+        this.filePath = "src/test/resources/" + fileName;
+    }
+
+    public void log(AbstractFunction fun) {
+        try {
+            Files.deleteIfExists(Paths.get(this.filePath));
+        } catch (IOException e) {
+        }
+
+        try (PrintStream printStream = new PrintStream(new FileOutputStream(filePath, true))) {
+            for (double i = start; i < end; i += step) {
+                printStream.printf("%f%s%f\n", i, separator, fun.calculate(i));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/main/java/guldilin/logger/Logger.java
+++
@@ -1,5 +0,0 @@
-package guldilin.logger;
-
-public interface Logger {
-    void log(String message);
-}
